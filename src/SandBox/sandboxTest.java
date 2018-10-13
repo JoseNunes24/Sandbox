3277b5e5322bac18e175cb90fab8a5e2de5748e5
Jose Nunes <pedrocosta24@live.com.pt>
2018-10-13 17:21:18 +0100

    Version control test

--- src/SandBox/sandboxTest.java
+++ src/SandBox/sandboxTest.java
@@ -2,6 +2,6 @@ package SandBox;
 
 public class sandboxTest {
     public static void main(String[] args) {
-        System.out.println("Hello Github!");
+        System.out.println("Hello Angela!");
     }
 }
